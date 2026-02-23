4922e213202675e94c6db7e16f623b692f82d312
Louislp19 <louislp19@hotmail.com>
2026-02-23 11:31:52 -0500

    fix python handler for hello

--- api/hello.py
+++ api/hello.py
@@ -1,6 +1,11 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({"message": "Python OK !"})
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        body = json.dumps({"message": "Python OK !"})
+        self.wfile.write(body.encode("utf-8"))
+        return
